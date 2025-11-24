1efab21e09ce99f19082df65d8bab21d99ce458c
wys-cse <wooys5796@gmail.com>
2025-11-24 22:41:59 +0900

    feat: 중복 투표 확인용 메서드 구현

--- src/main/java/com/everyones_coupon/repository/FeedbackRepository.java
+++ src/main/java/com/everyones_coupon/repository/FeedbackRepository.java
@@ -1,1 +1,10 @@
 package com.everyones_coupon.repository;
+
+import com.everyones_coupon.domain.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    
+    // 중복 투표 확인용 메서드 (exists 쿼리 사용으로 성능 최적화)
+    boolean existsByCouponIdAndIpAddress(Long couponId, String ipAddress);
+}
