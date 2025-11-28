ec38e05f6919c2a93c3f4cbb97a43015b7c9ea51
otherfit <211596113+otherfit@users.noreply.github.com>
2025-11-29 06:03:38 +0900

    Revert "Merge pull request #15 from 2025-DGU-SE-04/feature/admin-integration-test"
    This reverts commit a64e5aee63b41908cfd46ff898f5ade8b77e7421, reversing
    changes made to f6810293fece56a294e439830f8918948109db3a.

--- src/test/java/com/everyones_coupon/integration/AdminTokenInitializerIntegrationTest.java
+++ src/test/java/com/everyones_coupon/integration/AdminTokenInitializerIntegrationTest.java
@@ -5,14 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@ActiveProfiles("integration")
 @TestPropertySource(properties = {"app.admin.auto-generate:true"})
-public class AdminTokenInitializerIntegrationTest extends IntegrationTestBase {
+public class AdminTokenInitializerIntegrationTest {
 
     @Autowired
     private AdminTokenRepository adminTokenRepository;
