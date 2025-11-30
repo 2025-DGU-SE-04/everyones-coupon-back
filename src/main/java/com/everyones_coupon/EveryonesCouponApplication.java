a109c2afa717706842333acfe14300c169753eee
wys-cse <wooys5796@gmail.com>
2025-11-30 22:41:12 +0900

    fix: config 파일로 수정

--- src/main/java/com/everyones_coupon/EveryonesCouponApplication.java
+++ src/main/java/com/everyones_coupon/EveryonesCouponApplication.java
@@ -2,9 +2,7 @@ package com.everyones_coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class EveryonesCouponApplication {
 
